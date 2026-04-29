b3feb1cc75432a95f9586bf043c78b2106d33985
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2026-04-29 14:22:09 +0530

    keep alive

--- discord/main.py
+++ discord/main.py
@@ -1,9 +1,11 @@
 import credentials
 
 from src.bot import DiscordBot
+from src.keep_alive import keep_alive
 
 if __name__ == "__main__":
     try:
+        keep_alive()
         bot = DiscordBot()
 
         bot.run(credentials.TOKEN)
